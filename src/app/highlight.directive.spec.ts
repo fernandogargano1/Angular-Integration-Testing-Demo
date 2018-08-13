d9951643628b004ff251b822d5905700010fd318
Fernando Gargano <fernando@google.com>
2018-08-13 10:44:34 +0200

    Updated highlight directive

--- src/app/highlight.directive.spec.ts
+++ src/app/highlight.directive.spec.ts
@@ -28,25 +28,13 @@ describe('HighlightDirective', () => {
     fixture.detectChanges(); 
   });
 
-  it('should highlight the first element with cyan', () => {
-    /* 
-      So we are gonna get a reference to this element from 
-      an array of paragraph.
-     */
+  it('should highlight the first element with cyan', () => {    
     let de = fixture.debugElement.queryAll(By.css('p'))[0];
     expect(de.nativeElement.style.backgroundColor).toBe('cyan');
   });
 
   it('should highlight the first element with the default color', () => {
-    /* 
-      So we are gonna get a reference to this element from 
-      an array of paragraph. In order that the test doesn't 
-      break if we in the future change default color we 
-      get a reference to the directive (as we did we services
-      using the injector) and change our assertion from
-      yellow to directive dot default color. This way our 
-      test is more general.
-     */
+    
     let de = fixture.debugElement.queryAll(By.css('p'))[1];
     let directive = de.injector.get(HighlightDirective);
 
